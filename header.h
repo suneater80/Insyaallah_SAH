f9c4d4a8e39080c112e22849e92ab72ab5f4d0f9
suneater80 <hafizzulhakim142@gmail.com>
2025-06-13 10:31:29 +0700

    menambahkan kode untuk pasien, kunjungan, input/output, rekam medis

---
+++ header.h
@@ -0,0 +1,15 @@
+// Include semua header, yang nanti akan dipanggil di body
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Header/pasien.h"
+
+#include "Body/pasien.c"
+#include "Body/InputOutput.c"
+
+#endif
